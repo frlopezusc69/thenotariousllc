75b8306ae840f3ed2c44be6a084ed456dac155d3
Felix Lopez <felixlop@usc.edu>
2021-06-18 08:08:52 -0700

    working video background

--- src/components/Mainpart.js
+++ src/components/Mainpart.js
@@ -6,7 +6,7 @@ import './Mainpart.css';
 function Mainpart() {
     return (
         <div className='mainpart-container'>
-            {/* <video src='https://vod-progressive.akamaized.net/exp=1623843427~acl=%2Fvimeo-prod-skyfire-std-us%2F01%2F4036%2F16%2F420182206%2F1814364972.mp4~hmac=34926987314bea85b14d926b091383c71293c0feae1b92f67b8aa0da29047e00/vimeo-prod-skyfire-std-us/01/4036/16/420182206/1814364972.mp4?filename=production+ID%3A4432818.mp4' autoPlay loop muted /> */}
+            <video src='https://vod-progressive.akamaized.net/exp=1624043179~acl=%2Fvimeo-prod-skyfire-std-us%2F01%2F4036%2F16%2F420182206%2F1814364972.mp4~hmac=59cfc428eaff07314b0ba1bca65ab6657547f7f1eb8a0e64533579d95882d700/vimeo-prod-skyfire-std-us/01/4036/16/420182206/1814364972.mp4?filename=production+ID%3A4432818.mp4' autoPlay loop muted />
             <h1>It Was Not a Dream..</h1>
             <p>Your Notary Journey Starts Today!</p>
             <div className="mainpart-btns">
