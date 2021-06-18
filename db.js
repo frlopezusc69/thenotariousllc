6784aa4e3903408433ef5628d878ff52f3279e06
Felix Lopez <81645633+frlopezusc69@users.noreply.github.com>
2021-06-18 07:47:04 -0700

    commented out db file

--- db.js
+++ db.js
@@ -1,10 +1,10 @@
 const mysql = require("mysql("); 
 
-const db = mysql.createConnection({
-    host: ec2-18-233-83-165.compute-1.amazonaws.com
-    user: nteipbwnecttzj
-    password: a0c681547086a4febc8f17f314384d7c6cf3af9e3fa289d48c51efc09beba8ad
-    database: dfct3nq8g9kuoe
-}); 
+// const db = mysql.createConnection({
+    // host: ec2-18-233-83-165.compute-1.amazonaws.com
+    // user: nteipbwnecttzj
+    // password: a0c681547086a4febc8f17f314384d7c6cf3af9e3fa289d48c51efc09beba8ad
+    // database: dfct3nq8g9kuoe
+// }); 
 
-module.exports = db;
+// module.exports = db;
